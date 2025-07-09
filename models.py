48b1ad404b09f6c02bcb08bb067784e355dc76f3
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-07-09 16:27:02 +0700

    added the author and his books and established a connection between them

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
 from database import Base
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Table, ForeignKey
+from sqlalchemy.orm import relationship
+
+
 
 class Author(Base):
     __tablename__ = "authors"
@@ -7,3 +10,16 @@ class Author(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
     surname = Column(String(100), nullable=False)
+    books = relationship("Book",
+                         back_populates="author",
+                         cascade="all, delete-orphan",
+                         passive_deletes=True)
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    author_id = Column(Integer, ForeignKey("authors.id"), ondelete="CASCADE")
+    author = relationship("Author", back_populates="books")
+
