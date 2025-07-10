66fdd4d4f85abda5af8ed8c98edf8a5500793e7f
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-07-10 19:29:33 +0700

    add Annotated

--- library_project/app/schemas/book_schema.py
+++ library_project/app/schemas/book_schema.py
@@ -1,13 +1,14 @@
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, field_validator, Field
 from datetime import date
+from typing import Annotated
 
 
 class Book(BaseModel):
     title: str
-    year_of_manufacture: date = Field(
-        ...,
-        description="Год выпуска книги"
-    )
+    year_of_manufacture: Annotated[
+        date,
+        Field(description="Год выпуска книги")
+    ]
 
     @field_validator("year_of_manufacture")
     def check_year(cls, v: date) -> date:
