191c1b357167d7dbf450cc3ad0c7dca40478a1e6
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-07-16 11:03:59 +0700

    no more create all on app start

--- library_project/main.py
+++ library_project/main.py
@@ -2,14 +2,12 @@ import uvicorn
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
-from library_project.core_app.models import Base, db_helper
 from library_project.app.api import author_router, book_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+
     yield
 
 
