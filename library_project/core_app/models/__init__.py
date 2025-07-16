7f96c8f9c09b95fdd6c76daec99a3ef610538348
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-07-16 11:21:10 +0700

    create user table

--- library_project/core_app/models/__init__.py
+++ library_project/core_app/models/__init__.py
@@ -1,11 +1,6 @@
-__all__ = (
-    "Base",
-    "Author",
-    "Book",
-    "db_helper",
-    "DatabaseHelper",
-)
+__all__ = ("Base", "Author", "Book", "db_helper", "DatabaseHelper", "User")
 
 from .base import Base
 from .book_and_author import Book, Author
 from .db_helpers import db_helper, DatabaseHelper
+from .user import User
