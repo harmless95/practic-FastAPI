759359d8b00ae0a16f30301dde7da50532fe3a5d
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-07-16 11:34:05 +0700

    create post model

--- library_project/core_app/models/__init__.py
+++ library_project/core_app/models/__init__.py
@@ -1,6 +1,15 @@
-__all__ = ("Base", "Author", "Book", "db_helper", "DatabaseHelper", "User")
+__all__ = (
+    "Base",
+    "Author",
+    "Book",
+    "db_helper",
+    "DatabaseHelper",
+    "User",
+    "Post",
+)
 
 from .base import Base
 from .book_and_author import Book, Author
 from .db_helpers import db_helper, DatabaseHelper
 from .user import User
+from .post import Post
