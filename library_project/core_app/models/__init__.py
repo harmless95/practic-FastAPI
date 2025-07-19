b428bf383018f1ad82fa8c91a1e7d475be72ab89
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-07-19 22:09:27 +0700

    changed the path to new files with models

--- library_project/core_app/models/__init__.py
+++ library_project/core_app/models/__init__.py
@@ -10,7 +10,8 @@ __all__ = (
 )
 
 from .base import Base
-from .book_and_author import Book, Author
+from .book import Book
+from .author import Author
 from .db_helpers import db_helper, DatabaseHelper
 from .user import User
 from .post import Post
