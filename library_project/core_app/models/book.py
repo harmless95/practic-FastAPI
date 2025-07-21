de09c349c69f1c5c3233089b21b4bee168c7fd84
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-07-21 23:26:34 +0700

    add relationship book order

--- library_project/core_app/models/book.py
+++ library_project/core_app/models/book.py
@@ -1,13 +1,22 @@
 from datetime import date
-
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Integer, String
 
 from .base import Base
 from .mixins_author import MixinAuthor
+from .order_book_association import order_book_association_table
+
+if TYPE_CHECKING:
+    from .order import Order
 
 
 class Book(MixinAuthor, Base):
     _author_back_populates = "books"
 
-    title: Mapped[str] = mapped_column(nullable=False)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
     year_of_manufacture: Mapped[date] = mapped_column(nullable=False)
+    price: Mapped[int] = mapped_column(Integer, nullable=False)
+    orders: Mapped[list["Order"]] = relationship(
+        secondary=order_book_association_table, back_populates="books"
+    )
